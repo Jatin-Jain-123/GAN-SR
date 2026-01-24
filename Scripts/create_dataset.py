79d63835b754b24ec564416e9c71cf18044f3fb1
Jatin Jain <jatin321jain@gmail.com>
2026-01-25 03:49:07 +0530

    modified dataset creation script

--- Scripts/create_dataset.py
+++ Scripts/create_dataset.py
@@ -2,8 +2,10 @@ import torch
 import cv2
 from torch.utils.data import Dataset
 from pathlib import Path
+import random
 class DIV2K(Dataset):
-    def __init__(self, lr_dir, hr_dir):
+    def __init__(self, lr_dir, hr_dir,mode):
+        self.mode=mode
         self.lr_dir=Path(lr_dir)
         self.hr_dir=Path(hr_dir)
         self.filenames = sorted(set(p.name for p in self.hr_dir.iterdir()) & set(p.name for p in self.lr_dir.iterdir()))
@@ -25,9 +27,26 @@ class DIV2K(Dataset):
         """
         lr=cv2.cvtColor(lr,cv2.COLOR_BGR2RGB)
         hr=cv2.cvtColor(hr,cv2.COLOR_BGR2RGB)
-        lr=torch.from_numpy(lr)
-        lr=lr.permute(2,0,1)
-        lr=lr.float()
-        lr=lr/255.0
-        hr=torch.from_numpy(hr).permute(2,0,1).float()/255.0
-        return lr,hr
+        if self.mode=="train":
+            scale = 4
+            hr_patch = 96      # HR patch size
+            lr_patch = hr_patch // scale  # 96 / 4 = 24
+            h,w,_= hr.shape
+            hr_h=random.randint(0,h-hr_patch)
+            hr_w=random.randint(0,w-hr_patch)
+            hr=hr[hr_h:hr_h+hr_patch,hr_w:hr_w+hr_patch]
+            lr_h=hr_h//scale
+            lr_w=hr_w//scale
+            lr=lr[lr_h:lr_h+lr_patch,lr_w:lr_w+lr_patch]
+            lr=torch.from_numpy(lr).permute(2,0,1).float()/255.0
+            hr=torch.from_numpy(hr).permute(2,0,1).float()/255.0
+            return lr,hr
+        elif self.mode =="eval" or self.mode=="test":
+            lr=torch.from_numpy(lr)
+            lr=lr.permute(2,0,1)
+            lr=lr.float()
+            lr=lr/255.0
+            hr=torch.from_numpy(hr).permute(2,0,1).float()/255.0
+            return lr,hr
+        else:
+            raise ValueError(f"Mode is not specified {self.mode}")
--- inference/inference.py
+++ inference/inference.py
@@ -4,7 +4,7 @@ from Scripts.create_dataset import DIV2K
 root = Path(__file__).resolve().parent.parent/"dataset"/"val"
 lr_dir=root/"LR_x4"
 hr_dir=root/"HR"
-data=DIV2K(lr_dir,hr_dir)
+data=DIV2K(lr_dir,hr_dir,"eval")
 loader=DataLoader(data,batch_size=4,shuffle=False,num_workers=2,pin_memory=False)
 lr,hr = next(iter(loader))
 print(lr.shape,hr.shape)
